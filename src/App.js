de93e6a5395dacf742c33b1066367e12ac87afca
양정화 <siltarre@gmail.com>
2024-10-03 19:39:03 +0900

    useEffect로 초기데이터값 지정해주기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReviewList from "./components/ReviewList";
 import { getReviews } from "./api";
 
@@ -24,10 +24,15 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoadClick = async () => {
+  const handleLoad = async () => {
     const { reviews } = await getReviews();
     setItems(reviews);
   };
+
+  useEffect(() => {
+    handleLoad();
+  }, []);
+
   return (
     <div>
       <div>
@@ -35,7 +40,6 @@ function App() {
         <button onClick={handleBestClick}>인기순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
