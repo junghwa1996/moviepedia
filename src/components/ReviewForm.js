9f3abfd4a51a420fc59690fa4bc64fd16b8f63b5
양정화 <siltarre@gmail.com>
2024-10-07 16:41:20 +0900

    하나의 state로 여러값 관리하기

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -1,27 +1,30 @@
 import { useState } from "react";
 
 function ReviewForm() {
-  const [title, setTitle] = useState("");
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState("");
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+  });
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevValue) => ({
+      ...prevValue,
+      [name]: value,
+    }));
   };
 
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value) || 0;
-    setRating(nextRating);
-  };
-
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
   };
   return (
-    <form>
-      <input type='text' value={title} onChange={handleTitleChange} />
-      <input type='number' value={rating} onChange={handleRatingChange} />
-      <input type='text' value={content} onChange={handleContentChange} />
+    <form className='ReviewForm' onSubmit={handleSubmit}>
+      <input type='text' name='title' value={values.title} onChange={handleChange} />
+      <input type='number' name='rating' value={values.rating} onChange={handleChange} />
+      <input type='text' name='content' value={values.content} onChange={handleChange} />
+      <button type='submit'>확인</button>
     </form>
   );
 }
