26d12800fd5f6eb0934d43272f53b4d819574ec9
양정화 <siltarre@gmail.com>
2024-10-07 23:56:28 +0900

    useRef 를 이용한 DOM 속성 직접 가져오기

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,10 +1,26 @@
+import { useRef } from "react";
+
 function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
-  return <input type='file' onChange={handleChange} />;
+  const handleClearClick = () => {
+    inputRef.current.value = "";
+    onChange(name, null);
+  };
+
+  return (
+    <div>
+      <input type='file' ref={inputRef} onChange={handleChange} />
+      <button type='button' onClick={handleClearClick}>
+        삭제
+      </button>
+    </div>
+  );
 }
 
 export default FileInput;
